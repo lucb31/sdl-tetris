33121d2c750df6b119d4cf2550bca0a9d95b37aa
lucb31 <40357884+lucb31@users.noreply.github.com>
2025-05-23 20:28:53 +0200

    chore: Remove debugging code

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,6 @@ int main() {
     SDL_zero(event);
 
     // Debug FPS Calculation setup
-    constexpr int sample_size = 60;
-    Uint64 fps_calc_start_time = SDL_GetTicks();
     while (!quit) {
         const Uint64 frame_start_time = SDL_GetTicksNS();
 
@@ -37,23 +35,8 @@ int main() {
         // Render current frame
         renderer.render();
         frames++;
-        const Uint64 frame_render_time_ns = SDL_GetTicksNS() - frame_start_time;
-
-        // DEBUG: Log avg framerate
-        if (frames % sample_size == 0) {
-            const Uint64 end = SDL_GetTicks();
-            // time passed in milliseconds for 10000 frames
-            const Uint64 diff = end - fps_calc_start_time;
-
-            // *1000 to convert from frames per ms to frames per s
-            const float avg_fps = static_cast<double>(sample_size) / static_cast<double>(diff) * 1000.0f;
-            SDL_Log("FPS: %.1f", avg_fps);
-
-            // Update reference point
-            fps_calc_start_time = end;
-        }
-
         // Cap FPS: Delay render of next frame
+        const Uint64 frame_render_time_ns = SDL_GetTicksNS() - frame_start_time;
         const Uint64 wait_time_ns = nsPerFrame - frame_render_time_ns;
         const auto wait_time_ms = static_cast
                 <Uint32>(static_cast<double>(wait_time_ns) / 1e6);
